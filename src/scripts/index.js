2ceeff8fba9a0d4bc33c7e36988d6cff53714d30
RamS24 <AlSaadi.Rami24@gmail.com>
2022-09-29 17:31:22 +0200

    solution

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,10 +8,17 @@ let promiseOfModal = new Promise(function (resolve) {
     }, (1000 * 60));
 });
 
-promiseOfModal.then(function(val) {
+// promiseOfModal.then(function(val) {
+//     console.log("User has been on the page for 60 seconds");
+//     val.style.display = "block";
+// })
+//1.
+async function promiseFunc() {
+    let val = await promiseOfModal;
     console.log("User has been on the page for 60 seconds");
     val.style.display = "block";
-})
+}
+promiseFunc();
 
 modal.addEventListener("click", (e) => {
     switch(e.target.className) {
@@ -22,3 +29,31 @@ modal.addEventListener("click", (e) => {
     }
 })
 
+
+// 3. first way 
+// async function promiseBtn() {
+//     let promise2 = new Promise(function (resolve) {
+//         window.setTimeout(function () {
+//             let btn = document.querySelector('#continue');
+//             resolve(btn);
+//             console.log(btn.getAnimations()[0].finished)
+//         }, (300));
+//     });
+//     let val = await promise2;
+//     alert('Continue to subscribe');
+//     val.style.backgroundColor = '#eee';
+// }
+
+// document.querySelector('#continue').addEventListener('mouseover', (hover) => {
+//     promiseBtn();
+// })
+
+// 3. 2nd way
+async function promiseBtn() {
+    let btn = document.querySelector('#continue');
+    await btn.getAnimations()[0].finished;
+    alert('Continue to subscribe');
+    btn.style.backgroundColor = '#eee';
+}
+
+document.querySelector('#continue').addEventListener('mouseover', hover => promiseBtn())
